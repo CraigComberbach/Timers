d466e613fc3b7b62f17041540753e9e22e61716c
Craig Comberbach <craigc@price-hvac.com>
2013-08-08 16:34:22 -0500

    Enabled interrupts
    Interrupts are now turned on if a valid function pointer (Not a null
    pointer) is sent as an argument.
    The function pointer is also not set if the pointer is a null pointer.

--- Firmware/Timers.c
+++ Firmware/Timers.c
@@ -10,13 +10,13 @@ v0.1.0	2013-08-08  Craig Comberbach
 	Compiler: C30 v3.31	IDE: MPLABx 1.85	Tool: ICD3	Computer: Intel Core2 Quad CPU 2.40 GHz, 5 GB RAM, Windows 7 64 bit Home Premium SP1
 	Timers 1-4 initialization and interrupts are implemented for the PIC24F08KL200 chip
 		-Timer 3 is broken into two initializations, a gated and a standard function. I hope to combine these in the future
+	Interrupts are enabled and the function pointer are only set if the pointer sent is not a null pointer
 v0.0.0	2013-07-20  Craig Comberbach
 	Compiler: C30 v3.31	IDE: MPLABx 1.80	Tool: RealICE	Computer: Intel Xeon CPU 3.07 GHz, 6 GB RAM, Windows 7 64 bit Professional SP1
 	First version
 **************************************************************************************************/
 //TODO - Create functions to Set PR1/2/3/4
 //TODO - Create functions to turn On/Off each timer
-//TODO - Add to each timer the control bits to enable interrupts if they are nessecary
 //TODO - Add sentinels to the arguments passed to each function
 //TODO - Add to Project Setup.h a clock frequency definition to eliminate the need for setting pre/post scalers. This would require an argument asking the duration devision (S, mS, uS, ticks, etc) and a time
 /*************    Header Files    ***************/
@@ -56,13 +56,17 @@ int Initialize_TMR1(int prescale, void (*interruptFunction)(void))
 		T1CONbits.TSIDL	= 0;		//0 = Continue module operation in Idle mode
 		T1CONbits.TON	= 1;		//1 = Starts 16-bit Timer1
 	#elif defined PLACE_MICROCHIP_PART_NAME_HERE
-		return 0;//Timer2 does not exist on this chip, as such, this function call has failed
+		return 0;//Timer1 does not exist on this chip, as such, this function call has failed
 	#else
-		#warning "This timer1 is not setup for this chip"
+		#warning "Timer1 is not setup for this chip"
 	#endif
 
-	//Setup the function to call in the interrupt routine
-	TMR1_interruptFunction = interruptFunction;//It is valid
+	//Only setup the interrupts if we have a valid function pointer
+	if(interruptFunction)//Check for null pointer
+	{
+		TMR1_interruptFunction = interruptFunction;//Setup the function to call in the interrupt routine
+		IEC0bits.T1IE = 1;//Enable the interrupt
+	}
 
 	//Success
 	return 1;
@@ -78,11 +82,15 @@ int Initialize_TMR2(int prescale, int postscale, void (*interruptFunction)(void)
 	#elif defined PLACE_MICROCHIP_PART_NAME_HERE
 		return 0;//Timer2 does not exist on this chip, as such, this function call has failed
 	#else
-		#warning "This timer1 is not setup for this chip"
+		#warning "Timer2 is not setup for this chip"
 	#endif
 
-	//Setup the function to call in the interrupt routine
-	TMR2_interruptFunction = interruptFunction;
+	//Only setup the interrupts if we have a valid function pointer
+	if(interruptFunction)//Check for null pointer
+	{
+		TMR2_interruptFunction = interruptFunction;//Setup the function to call in the interrupt routine
+		IEC0bits.T2IE = 1;//Enable the interrupt
+	}
 
 	//Success
 	return 1;
@@ -110,11 +118,15 @@ int Initialize_TMR3_As_Timer(int prescale, void (*interruptFunction)(void))
 	#elif defined PLACE_MICROCHIP_PART_NAME_HERE
 		return 0;//Timer3 does not exist on this chip, as such, this function call has failed
 	#else
-		#warning "This timer1 is not setup for this chip"
+		#warning "Timer3 is not setup for this chip"
 	#endif
 
-	//Setup the function to call in the interrupt routine
-	TMR3_interruptFunction = interruptFunction;
+	//Only setup the interrupts if we have a valid function pointer
+	if(interruptFunction)//Check for null pointer
+	{
+		TMR3_interruptFunction = interruptFunction;//Setup the function to call in the interrupt routine
+		IEC0bits.T3IE = 1;//Enable the interrupt
+	}
 
 	//Success
 	return 1;
@@ -142,11 +154,15 @@ int Initialize_TMR3_As_Gated_Timer(int prescale, int gateSource, int mode, int t
 	#elif defined PLACE_MICROCHIP_PART_NAME_HERE
 		return 0;//Timer3 does not exist on this chip, as such, this function call has failed
 	#else
-		#warning "This timer1 is not setup for this chip"
+		#warning "Timer3 is not setup for this chip"
 	#endif
 
-	//Setup the function to call in the interrupt routine
-	TMR3_interruptFunction = interruptFunction;
+	//Only setup the interrupts if we have a valid function pointer
+	if(interruptFunction)//Check for null pointer
+	{
+		TMR3_interruptFunction = interruptFunction;//Setup the function to call in the interrupt routine
+		IEC0bits.T3IE = 1;//Enable the interrupt
+	}
 
 	//Success
 	return 1;
@@ -162,11 +178,21 @@ int Initialize_TMR4(int prescale, int postscale, void (*interruptFunction)(void)
 		T4CONbits.T4CKPS	= prescale;	//Timer4 Clock Prescale Select bits (0 = 1:1, 1 = 1:4, 2 = 1:16, 3 = Undefined)
 		T4CONbits.TON		= 1;		//1 = Timer4 is on
 	#else
-		#warning "This timer1 is not setup for this chip"
+		#warning "Timer4 is not setup for this chip"
 	#endif
 
-	//Setup the function to call in the interrupt routine
-	TMR4_interruptFunction = interruptFunction;
+	//Only setup the interrupts if we have a valid function pointer
+	if(interruptFunction)//Check for null pointer
+	{
+		TMR4_interruptFunction = interruptFunction;//Setup the function to call in the interrupt routine
+		#if defined __PIC24F08KL200__
+			return 0;//Timer4 does not exist on this chip, as such, this function call has failed
+		#elif define PLACE_MICROCHIP_PART_NAME_HERE
+			IEC1bits.T1IE = 1;//Enable the interrupt
+		#else
+			#warning "Timer4 is not setup for this chip"
+		#endif
+	}
 
 	//Success
 	return 1;
